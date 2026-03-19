426eec65f0da745aba2017272832ae98d5b39128
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-19 23:31:38 +0330

    Make solution structure

---
+++ SolbeIt.Common/OperationResult/OperationResult.cs
@@ -0,0 +1,57 @@
+﻿namespace SolbeIt.Common.OperationResult;
+public class OperationResult<TEntity>
+{
+	#region Constructor
+	public OperationResult(bool isSuccess, TEntity? data, string? message, List<ModelStateError> modelStateErrors) : base()
+	{
+		IsSuccess = isSuccess;
+		Message = message;
+		Data = data!;
+		ModelStateErrors = modelStateErrors;
+	}
+
+	public OperationResult(bool isSuccess, TEntity? data, string? message, ModelStateError? modelStateError = null) : base()
+	{
+		IsSuccess = isSuccess;
+		Message = message;
+		Data = data!;
+		ModelStateErrors = modelStateError == null ? null : new List<ModelStateError>
+		{
+			modelStateError
+		};
+	}
+	#endregion Constructor
+
+	#region Properties
+	public bool IsSuccess { get; set; }
+	public TEntity? Data { get; set; }
+	public string? Message { get; set; }
+	public List<ModelStateError>? ModelStateErrors { get; set; }
+
+	#endregion Properties
+
+
+}
+public class ModelStateError
+{
+	public string ModelStateField { get; set; }
+	public string ModelStateErrorMessage { get; set; }
+	#region Methods
+	public static ModelStateError MakeModelStateError(string field, string message)
+	{
+		return new ModelStateError()
+		{
+			ModelStateField = field,
+			ModelStateErrorMessage = message
+		};
+	}
+	#endregion Methods
+
+}
+
+public enum ErrorMessageEnum
+{
+	UserNotFound = 1,
+
+	SomethingWentWrong = 999
+}
