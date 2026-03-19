a992b255cda1856dbdeadd48a53325b1434222d8
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 02:50:53 +0330

    Add a base controller

---
+++ SolveIt.Web/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolveIt.Web.Controllers;
+public class BaseController : Controller{}
+
+[Authorize]
+public class BaseAhtorizeController : BaseController{}
