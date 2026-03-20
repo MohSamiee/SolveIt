18560e0ed4a13f51ff26a23cee5771d1b0a60f5b
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 08:04:40 +0330

    add method for generate email activation code

---
+++ SolveIt.Common/Generators/CodeGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace BestShop.Common.Generator;
+public static class CodeGenerator
+{
+	public static string GenerateActivationEmailCode()
+	{
+		var res = Guid.NewGuid().ToString("N");
+		return res;
+	}
+}
