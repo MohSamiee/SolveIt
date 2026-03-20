1506a6adb5d71bef85d783bddf65ced477db6575
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 08:03:29 +0330

    Add string convertor class

---
+++ SolveIt.Common/Convertors/StringConvertor.cs
@@ -0,0 +1,22 @@
+﻿namespace SolveIt.Common.String;
+public static class StringConvertor
+{
+	public static string DecorateStringForRoute(this string value)
+	{
+		return value.Replace(" ", "_");
+	}
+
+	public static string ShortenLongText(this string longText, int maxChar, int thresholdLimitChar, string suffixString = "...")
+	{
+		return
+			longText.Length < thresholdLimitChar
+			? longText
+			: longText.Substring(0, maxChar) + suffixString;
+	}
+
+	public static string StringNormalize(this string value)
+	{
+		var res = value.Trim().ToUpper();
+		return res;
+	}
+}
