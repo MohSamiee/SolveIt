aab275c7dbd2003d0aa5687491c98595b09f4b21
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 08:06:09 +0330

    add a method for model validation

--- SolveIt.Application/GlobalUsings.cs
+++ SolveIt.Application/GlobalUsings.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using SolveIt.Application.Services.Interfaces.Accounts;
+global using SolveIt.Application.ViewModels.Accounts;
+global using SolveIt.Common.OperationResult;
+global using SolveIt.Common.String;
+global using SolveIt.Domain.Interfaces.Accounts;
+global using SolveIt.Entities.Models.Users;
+global using SolveIt.Common.Resources;
---
+++ SolveIt.Application/ViewModels/ModelVerification.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestShop.Data.ViewModels;
+public class ModelVerification
+{
+	public async Task<OperationResult<bool, ValidationResultEnum>> ModelValidation(object model)
+	{
+		var validations = new List<ValidationResult>();
+		var errors = new List<ModelStateError>();
+		if (model == null)
+			return new OperationResult<bool, ValidationResultEnum>(false, false, PropertyDictionary.GnSomethingWenWrong, ValidationResultEnum.Failed, ModelStateError.MakeModelStateError("", PropertyDictionary.GnSomethingWenWrong));
+
+		var context = new ValidationContext(model);
+		var res = Validator.TryValidateObject(
+			model,
+			context,
+			validations,
+			validateAllProperties: true
+		);
+		if (res)
+			return new OperationResult<bool, ValidationResultEnum>(true, true, "",ValidationResultEnum.Success);
+
+		foreach (var item in validations)
+		{
+			foreach (var err in item.MemberNames)
+			{
+				errors.Add(ModelStateError.MakeModelStateError(err, item.ErrorMessage ?? ""));
+			}
+		}
+
+		return new OperationResult<bool, ValidationResultEnum>(false, false, PropertyDictionary.GnSomethingWenWrong, ValidationResultEnum.Failed, errors);
+	}
+}
+
+public enum ValidationResultEnum
+{
+	Success = 0,
+	Failed =1
+}
