ca35beb9e9a8839eb61206b04d794c6b4921a384
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 06:44:14 +0330

    Change in operation result to return the enum result

--- SolveIt.Common/OperationResult/OperationResult.cs
+++ SolveIt.Common/OperationResult/OperationResult.cs
@@ -1,41 +1,53 @@
 ﻿namespace SolveIt.Common.OperationResult;
-public class OperationResult<TEntity>
+public class OperationResult<TEntity, TStatus> where TStatus : Enum
 {
-	#region Constructor
-	public OperationResult(bool isSuccess, TEntity? data, string? message, List<ModelStateError> modelStateErrors) : base()
-	{
-		IsSuccess = isSuccess;
-		Message = message;
-		Data = data!;
-		ModelStateErrors = modelStateErrors;
-	}
-
-	public OperationResult(bool isSuccess, TEntity? data, string? message, ModelStateError? modelStateError = null) : base()
-	{
-		IsSuccess = isSuccess;
-		Message = message;
-		Data = data!;
-		ModelStateErrors = modelStateError == null ? null : new List<ModelStateError>
-		{
-			modelStateError
-		};
-	}
-	#endregion Constructor
-
 	#region Properties
 	public bool IsSuccess { get; set; }
 	public TEntity? Data { get; set; }
 	public string? Message { get; set; }
 	public List<ModelStateError>? ModelStateErrors { get; set; }
-
+	public TStatus Status { get; set; }
 	#endregion Properties
 
+	#region Constructor
+	public OperationResult(
+		bool isSuccess,
+		TEntity? data,
+		string? message,
+		TStatus status,
+		List<ModelStateError> modelStateErrors) : base()
+	{
+		this.IsSuccess = isSuccess;
+		this.Message = message;
+		this.Data = data!;
+		this.Status = status;
+		this.ModelStateErrors = modelStateErrors;
+	}
 
+	public OperationResult(
+		bool isSuccess,
+		TEntity? data,
+		string? message,
+		TStatus status,
+		ModelStateError? modelStateError = null) : base()
+	{
+		this.IsSuccess = isSuccess;
+		this.Message = message;
+		this.Data = data!;
+		this.Status = status;
+		this.ModelStateErrors = modelStateError == null ? null : new List<ModelStateError>
+		{
+			modelStateError
+		};
+	}
+	#endregion Constructor
 }
+
 public class ModelStateError
 {
 	public string ModelStateField { get; set; }
 	public string ModelStateErrorMessage { get; set; }
+	
 	#region Methods
 	public static ModelStateError MakeModelStateError(string field, string message)
 	{
@@ -46,12 +58,4 @@ public class ModelStateError
 		};
 	}
 	#endregion Methods
-
-}
-
-public enum ErrorMessageEnum
-{
-	UserNotFound = 1,
-
-	SomethingWentWrong = 999
 }
