e8b9e09b99d03ba6650df30f3143be8057950042
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 08:05:38 +0330

    create path tools class

---
+++ SolveIt.Application/Statics/PathTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveIt.Application.Statics;
+public static class PathTools
+{
+	#region User
+	public static readonly string DefaultUserAvatar = "/content/user-avatar/DefaultAvatar.PNG";
+	#endregion User
+}
