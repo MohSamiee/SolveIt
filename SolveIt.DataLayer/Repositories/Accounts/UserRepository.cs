206d366aad1429c7a170f2d934f04c7f757cd624
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-20 08:05:12 +0330

    add user repository

---
+++ SolveIt.DataLayer/Repositories/Accounts/UserRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace SolveIt.DataLayer.Repositories.Accounts;
+public class UserRepository : GenericRepository<User>, IUserRepository
+{
+	public UserRepository(SolveItDbContext context) : base(context)
+	{
+	}
+}
---
+++ SolveIt.Domain/Interfaces/Accounts/IUserRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace SolveIt.Domain.Interfaces.Accounts;
+public interface IUserRepository : IGenericRepository<User>
+{
+
+}
