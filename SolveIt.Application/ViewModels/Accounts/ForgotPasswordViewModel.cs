917a774805dc21b4be34047040d36e79dc25096c
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-24 01:32:07 +0330

    Cleanup Reset password response

--- SolveIt.Application/ViewModels/Accounts/ForgotPasswordViewModel.cs
+++ SolveIt.Application/ViewModels/Accounts/ForgotPasswordViewModel.cs
@@ -4,8 +4,8 @@ namespace SolveIt.Application.ViewModels.Accounts;
 public class ForgotPasswordViewModel
 {
 	public string? Email { get; set; }
-	
-	
+
+
 	public string? Mobile { get; set; }
 
 
@@ -19,4 +19,27 @@ public class ForgotPasswordViewModel
 	[MaxLength(100, ErrorMessageResourceName = "GnMaxLengthErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
 	[Required(ErrorMessageResourceName = "GnRequiredErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
 	public string EmailOrMobile { get; set; }
+}
+
+public class ForgotPasswordResponseViewModel
+{
+	public EmailForgotPasswordResponseViewModel EmailData { get; set; }
+	public MobileForgotPasswordResponseViewModel MobileData { get; set; }
+	public ForgotPasswordResponseEnum ResponseType { get; set; }
+}
+public class EmailForgotPasswordResponseViewModel
+{
+	public string Email { get; set; }
+}
+
+public class MobileForgotPasswordResponseViewModel
+{
+	public string Mobile { get; set; }
+	public int CodeLength { get; set; }
+	public DateTime ExpireDateTime { get; set; }
+}
+public enum ForgotPasswordResponseEnum
+{
+	Mobile,
+	Email
 }
