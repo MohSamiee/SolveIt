94202e70e40135608a00ccc5433ffac251c0b51a
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-24 07:11:17 +0330

    change _UserSidebar to view component

---
+++ SolveIt.Web/Areas/UserPanel/Components/UserPanelSidebarViewComponent.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolveIt.Web.Areas.UserPanel.Components;
+public class UserPanelSidebarViewComponent : ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		return View();
+	}
+}
