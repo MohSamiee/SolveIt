8817b5b24ceb242105cf8ac1a6362c0ba43f47a1
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-03-26 19:47:00 +0330

    Add repository to get city and country

--- SolveIt.DataLayer/Repositories/Locations/StateRepository.cs
+++ SolveIt.DataLayer/Repositories/Locations/StateRepository.cs
@@ -1,8 +1,29 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+
 namespace SolveIt.DataLayer.Repositories.Accounts;
 public class StateRepository : GenericRepository<State>, IStateRepository
 {
 	public StateRepository(SolveItDbContext context) : base(context)
 	{
 	}
+
+	#region Countries
+
+	public async Task<List<State>> GetCountries()
+	{
+		var countryQuery = GetEntity(a => a.ParentId == null);
+		return await countryQuery.ToListAsync();
+	}
+	#endregion Countries
+
+	#region Cities
+	public async Task<List<State>> GetCities(long? countryId)
+	{
+		var cityQuery = GetEntity();
+		if ((countryId ?? 0) == 0)
+			cityQuery = cityQuery.Where(a => a.ParentId == countryId);
+		return await cityQuery.ToListAsync();
+	}
+	#endregion Cities
 }
--- SolveIt.Domain/Interfaces/Locations/IStateRepository.cs
+++ SolveIt.Domain/Interfaces/Locations/IStateRepository.cs
@@ -1,5 +1,14 @@
-﻿namespace SolveIt.Domain.Interfaces.Locations;
+﻿using System.Net.Sockets;
+
+namespace SolveIt.Domain.Interfaces.Locations;
 public interface IStateRepository : IGenericRepository<State>
 {
+	#region Counteries
+	Task<List<State>> GetCountries();
+	#endregion
+
+	#region Cities
+	Task<List<State>> GetCities(long? countryId);
+	#endregion Cities
 
 }
